a9882acaec06cf941b8bf293cd94e47705deb2a5
simon <vintrob.simon@gmail.com>
2023-10-20 21:12:08 -0300

    mas o menos andando la image detection

--- info.md
+++ info.md
@@ -5,5 +5,26 @@ bueno. varias partes:
 
 #### Detección de movimiento
 
-- Comparar imagen con imagen anterior, con cierto treshold.
-- Obtener UN punto de movimiento.
+*  Comparar imagen con imagen anterior, con cierto treshold.
+* Obtener UN punto de movimiento por cada objeto en movimiento. O sea:
+- Centro de bounding box
+- Algún cálculo maquiavélico para obtener el promedio entre todos los puntos que conforman un polígono
+* Librerías que pueden ayudar:
+- tracking.js
+
+##### Por qué CV o cualquier modelo de vision AI es overkill:
+* No necesito saber qué objeto estoy detectando
+* Quiero que esto sea muy lightweight (as in, que pueda correr en la cristinet o en una raspi crota en tiempo real)
+* El fondo y los participantes van a tener colores muy distintos, aparte el fondo no se va a mover y los participantes sí.
+* Simplemente necesitaría un algoritmo que tome dos imágenes, saque los píxeles de diferencia, y por cada cúmulo de píxeles que cambiaron calcule un punto medio.
+
+#### Fase lógica
+* Implementar lógica de notas. Qué genera una nota? Con qué intensidad, pitch, volumen, paneo?
+
+#### Conectar a MIDI
+* Pretty self-explanatory.
+* Librerías que pueden ayudar:
+- virmidi (apt)
+- midi (npm)
+
+
--- test.js
+++ test.js
@@ -1,59 +1,13 @@
 import fs from "fs";
 import { PNG } from "pngjs";
 import pixelmatch from "pixelmatch";
-// const pixelmatch = require("pixelmatch");
-//
-// const img1 = PNG.sync.read(fs.readFileSync("img1.png"));
-// const img2 = PNG.sync.read(fs.readFileSync("img2.png"));
-// const { width, height } = img1;
-// const diff = new PNG({ width, height });
-//
-// pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
-//
-// fs.writeFileSync("diff.png", PNG.sync.write(diff));
+const pixelmatch = require("pixelmatch");
 
-import NodeWebcam from "node-webcam";
-import Jimp from "jimp";
+const img1 = PNG.sync.read(fs.readFileSync("img1.png"));
+const img2 = PNG.sync.read(fs.readFileSync("img2.png"));
+const { width, height } = img1;
+const diff = new PNG({ width, height });
 
-//Default options
+pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
 
-const width = 1280,
-  height = 720;
-var opts = {
-  width,
-  height,
-  quality: 50,
-  frames: 1,
-  delay: 0,
-  output: "png",
-  device: false,
-  callbackReturn: "buffer",
-  verbose: false,
-};
-
-var Webcam = NodeWebcam.create(opts);
-
-const capt = async () => {
-  let arr = [];
-  for (let i = 0; i < 2; i++) {
-    let j = await new Promise((res, rej) => {
-      Webcam.capture("", function(err, data) {
-        if (err) rej(err);
-        res(data);
-      });
-    });
-    arr.push(j);
-  }
-
-  const jimage1 = await Jimp.read(arr[0]);
-  const jimage2 = await Jimp.read(arr[1]);
-  const w = jimage1.bitmap.width;
-  const h = jimage1.bitmap.height;
-  const diff = new PNG({ width, height });
-  pixelmatch(jimage1.bitmap.data, jimage2.bitmap.data, diff.data, w, h, {
-    threshold: 0.2,
-  });
-  fs.writeFileSync("diff.png", PNG.sync.write(diff));
-};
-
-capt();
+fs.writeFileSync("diff.png", PNG.sync.write(diff));
